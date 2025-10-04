798461e8f32f07bbb1bd8487601726bf00740674
pandarudra <rudrapanda8206@gmail.com>
2025-10-04 16:04:39 +0530

    updated

--- apps/web/src/app/Provider.tsx
+++ apps/web/src/app/Provider.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
+import { Session } from "next-auth";
 
-type Props = { children: ReactNode; session?: any };
+type Props = { children: ReactNode; session?: Session | null };
 
 export default function Providers({ children, session }: Props) {
   return <SessionProvider session={session}>{children}</SessionProvider>;
